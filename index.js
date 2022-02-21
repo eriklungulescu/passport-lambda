9412319ac1b4d57b00479cc567e679276c4918b7
eriklungulescu <eriklungulescu@outlook.com>
2022-02-21 11:59:46 -0500

    base functionality

--- index.js
+++ index.js
@@ -4,26 +4,25 @@ const ddbClient = new AWS.DynamoDB.DocumentClient({'region' : 'us-east-1'});
 
 
 api.get('/contacts', (req, res) => {
-    console.info("Request: " + JSON.stringify(req));
-    res.JSON("Successfully hit get endpoint");
+    console.info("Successfully hit get /contacts");
+    res.status(200).send({Response : "Successfully hit get /contacts"});
 })
 
 api.get('/contacts/:userId', (req, res) => {
-    console.info("Request: " + JSON.stringify(req));
-    res.JSON("Successfully hit get with userId" + req.params.userId + "endpoint");
+    console.info("Successfully hit get /contacts/" + req.params.userId);
+    res.status(200).send({Response : "Successfully hit get /contacts/" + req.params.userId});
 })
 
 api.post('/contacts', (req, res) => {
-    console.info("Request: " + JSON.stringify(req));
-    res.JSON("Successfully hit post endpoint")
+    console.info("Successfully hit post /contacts");
+    res.status(200).send({Response : "Successfully hit post /contacts"});
 })
 
-module.exports = async (event, context) => {
-    return await api.run(event, context, (error) => {
-        if (error) {
-            console.error("Encountered error: " + error);
-        } else {
-            console.info("Successfully processed request");
-        }
-    })
+api.any('/', (req, res) => {
+    console.info("Request hit unknown endpoint");
+    res.status(400).send("Uh Oh! Endpoint does not exist!")
+})
+
+exports.handler = async (event, context) => {
+    return await api.run(event, context)
 }
